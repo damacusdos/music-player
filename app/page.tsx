f962394cd083e2b3eeb37f612ad5ac5e228641b9
alison <71641175+damacusdos@users.noreply.github.com>
2024-07-10 21:54:55 +0800

    add tailwind and svgr

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import PlaySVG from '@/public/assets/play.svg';
+
 export default function Home() {
   return (
-    <main className="flex">
+    <main className="flex text-teal-900">
       <h1>Music Player</h1>
+      <PlaySVG />
     </main>
   );
 }
